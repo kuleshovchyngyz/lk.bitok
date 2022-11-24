4dc1c1ce6940bafc7fc9064c38659509c660a04d
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-11-24 17:27:37 +0600

    postman

--- app/Http/Controllers/Api/AddedUserController.php
+++ app/Http/Controllers/Api/AddedUserController.php
@@ -32,7 +32,7 @@ class AddedUserController extends Controller
         if (isset($country['id'])) {
             return AddedUserResource::collection($country->addedUsers);
         }
-        return AddedUserResource::collection(AddedUser::with('country','userOperations'));
+        return AddedUserResource::collection(AddedUser::with('country'));
     }
 
     /**
