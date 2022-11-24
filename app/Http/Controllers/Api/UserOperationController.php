96135359adcb5c4be65086dbcb954d09122b633d
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-11-24 17:34:00 +0600

    removing pagination

--- app/Http/Controllers/Api/UserOperationController.php
+++ app/Http/Controllers/Api/UserOperationController.php
@@ -34,7 +34,7 @@ class UserOperationController extends Controller
             }
             return UserOperationResource::collection($addedUser->userOperations);
         }
-        return UserOperationResource::collection(UserOperation::with('addedUser')->paginate(50));
+        return UserOperationResource::collection(UserOperation::with('addedUser')->get());
     }
 
     /**
