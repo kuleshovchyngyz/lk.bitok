1b2f961e2b967d085bef26625744a91b0a65f48b
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-12-05 21:07:36 +0600

    worker, Seeder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Storage::disk('local')->put('example.txt', now()->format('h:i'));
+        })->everyMinute();
     }
 
     /**
