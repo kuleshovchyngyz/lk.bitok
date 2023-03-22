1747f0ec22aa7b3f27f7105846f5dce1d93782f0
maksat607 <kuleshov.chyngyz@gmail.com>
2023-03-22 18:39:00 +0600

    change risk high risk columns

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,14 +10,7 @@ class Setting extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    protected $dates = ['high_risk','risk'];
 
-    public function setHighRiskAttribute($value)
-    {
-        $this->attributes['high_risk'] = Carbon::createFromFormat('d/m/Y',$value)->format('Y-m-d');
-    }
-    public function setRiskAttribute($value)
-    {
-        $this->attributes['risk'] = Carbon::createFromFormat('d/m/Y',$value)->format('Y-m-d');
-    }
+
+
 }
