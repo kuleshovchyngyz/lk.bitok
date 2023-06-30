f9bb2eb4a1d176eb220aa430180316c8c5b40b6a
Azamat <azamailnet@mail.ru>
2023-06-30 11:58:52 +0600

    this is the latest commit with paginations and roles, roles are not finished yet

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('auth/me', [AuthController::class,'me']);
     Route::get('auth/logout', [AuthController::class,'logout']);
     Route::resource('added-users', AddedUserController::class);
-    Route::resource('users', UserController::class);
+    Route::resource('users', UserController::class)->middleware('role:Admin');
     Route::resource('legal-entities', \App\Http\Controllers\Api\LegalEntityController::class);
     Route::resource('user-operations', UserOperationController::class);
     Route::resource('settings', \App\Http\Controllers\Api\SettingController::class);
