7d6bd61babd573577eb4de07cf330795629fee84
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-07-06 17:55:44 +0600

    Workbranch (#25)
    * roles from beginning
    
    * roles for user and addeduser
    
    * roles final
    
    * htaccess
    
    * fixing
    
    * action logs events
    
    * fixing
    
    * fixing show in AddedUsers
    
    * logs added to added_users, legals, user_operations
    
    * unnecessary migration create_permission_tables removed
    
    * made some changes
    
    * show user role and user status
    
    * changes from Maksat and my users status
    
    * fixed user update method
    
    * fixed userController
    
    * in logs filter by date the date format changed
    
    ---------
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- app/Http/Controllers/Api/LogController.php
+++ app/Http/Controllers/Api/LogController.php
@@ -17,10 +17,21 @@ class LogController extends Controller
         $query = Log::with('user')->latest();
         
         if ($request->has('start_date') && $request->has('end_date')) {
-            $start_date = $request->start_date;
-            $end_date = $request->end_date;
-            
-            $query->whereBetween('created_at', [$start_date, $end_date]);
+            $start_date = \DateTime::createFromFormat('d/m/Y', $request->input('start_date'))->format('Y-m-d');
+            $end_date = \DateTime::createFromFormat('d/m/Y', $request->input('end_date'))->format('Y-m-d');
+
+            $query->whereDate('created_at', '>=', $start_date)
+                ->whereDate('created_at', '<=', $end_date);
+        }
+        elseif ($request->has('start_date')) {
+            $start_date = \DateTime::createFromFormat('d/m/Y', $request->input('start_date'))->format('Y-m-d');
+        
+            $query->whereDate('created_at', '>=', $start_date);
+        } 
+        elseif ($request->has('end_date')) {
+            $end_date = \DateTime::createFromFormat('d/m/Y', $request->input('end_date'))->format('Y-m-d');
+        
+            $query->whereDate('created_at', '<=', $end_date);
         }
         
         $logs = $query->paginate(100);
