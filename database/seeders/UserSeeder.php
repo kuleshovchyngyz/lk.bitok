f76466c2fae9a5121a9da364b4ccb2a46e2ac171
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-07-13 13:30:38 +0600

    Ciwork (#74)
    * test again
    
    * changed workflow from roles to ci
    
    * again
    
    * test
    
    * test again
    
    * changed date
    
    * date format problems
    
    * dates
    
    * dates
    
    * dates again
    
    * hash faker
    
    * useroper date fixed
    
    * migrate spatie
    
    * composer update to run spatie migrations
    
    * spatie permissions with vendor publish
    
    * modified Role and permission seeder
    
    * blabla
    
    * haha
    
    * changed user in seeder for authorization
    
    * some fix
    
    * modified user seeder
    
    ---------
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,12 +18,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('0123456789'),
-            'role' => 'Admin',
-            'status' => 1,
-        ]);
+        User::factory()->times(1)->create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('0123456789'),
+                'role' => 'Admin',
+                'status' => 1,
+            ]
+        );
     }
 }
