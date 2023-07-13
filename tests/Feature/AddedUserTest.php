c6d434e0940d69d1c46ab3c898bffd7c3469f36f
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-07-13 13:43:46 +0600

    Ciwork (#75)
    * test again
    
    * changed workflow from roles to ci
    
    * again
    
    * test
    
    * test again
    
    * changed date
    
    * date format problems
    
    * dates
    
    * dates
    
    * dates again
    
    * hash faker
    
    * useroper date fixed
    
    * migrate spatie
    
    * composer update to run spatie migrations
    
    * spatie permissions with vendor publish
    
    * modified Role and permission seeder
    
    * blabla
    
    * haha
    
    * changed user in seeder for authorization
    
    * some fix
    
    * modified user seeder
    
    * fixing
    
    ---------
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- tests/Feature/AddedUserTest.php
+++ tests/Feature/AddedUserTest.php
@@ -5,8 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\AddedUser;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AddedUserTest extends TestCase
 {
