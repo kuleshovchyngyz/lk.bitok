1e8fcaa2b6b4568637e2753ace858cab0b3ca919
Azamat <azamailnet@mail.ru>
2023-07-14 17:43:23 +0600

    countrySeeder on

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([AddedUserSeeder::class,
                     BlackListSeeder::class,
+                    CountrySeeder::class,
                     UserSeeder::class,
                     RolesAndPermissionsSeeder::class,
                     UserOperationSeeder::class]);
