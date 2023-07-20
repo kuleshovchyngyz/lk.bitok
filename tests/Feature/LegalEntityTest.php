b3896b2605d5d335e744fda4dfd086e5cc716c8d
Azamat <azamailnet@mail.ru>
2023-07-20 14:41:44 +0600

    improving

--- tests/Feature/LegalEntityTest.php
+++ tests/Feature/LegalEntityTest.php
@@ -69,44 +69,134 @@ class LegalEntityTest extends TestCase
 
         $response->assertStatus(201); // Assert that the response has a status code of 201 (Created)
         $this->assertDatabaseHas('legal_entities', ['director_full_name' => 'John Doe']); // Assert that the data is stored in the database
+    }
+
+    public function testLegalEntityRequiredFields()
+    {  
+        $credentials = [];
+        
+        $response = $this->postJson('api/legal-entities', $credentials);
+
+        $response->assertStatus(422);
+        $response->assertSessionDoesntHaveErrors(['name', 'address', 'director_full_name', 'birth_date', 'country_id']);    
+    }
+
+    public function testStockStore()
+    {  
+        $credentials = [
+            'name' => 'New Company',
+            'address' => 'John Street 3',
+            'stock' => 1,
+            'iban' => 'LI61 0881 1010 2720 K000 U',
+            'bank_account' => '0102720001000840',
+            'bank_name' => 'Good Bank',
+            'swift' => 'BFRILI22XXX',
+            'account_code' => '001',
+        ];
+        
+        $response = $this->postJson('api/legal-entities', $credentials);
+
+        $response->assertStatus(201); // Assert that the response has a status code of 201 (Created)
+        $this->assertDatabaseHas('legal_entities', ['swift' => 'BFRILI22XXX']); // Assert that the data is stored in the database
+    
+    }
+
+    public function testStockRequiredFields()
+    {  
+        $credentials = [
+            'stock' => 1,
+            'iban' => 'LI61 0881 1010 2720 K000 U',
+            'bank_account' => '0102720001000840',
+            'bank_name' => 'Good Bank',
+            'swift' => 'BFRILI22XXX',
+        ];
+        
+        $response = $this->postJson('api/legal-entities', $credentials);
+
+        $response->assertStatus(422);
+        $response->assertSessionDoesntHaveErrors(['name', 'address', 'account_code']);
     
     }
 
     // testing update method
-    // public function testLegalEntityUpdate()
-    // {  
-    //     $legalEntity = LegalEntity::factory()->create(
-    //         [
-    //             'name' => 'New Company',
-    //             'director_full_name' => 'John Doe',
-    //             'birth_date' => '20/04/1981',
-    //             'country_id' => '2',
-    //             'address' => 'John Street 3',
-    //         ]
-    //     );
+    public function testLegalEntityUpdate()
+    {  
+        $legalEntity = LegalEntity::factory()->create(
+            [
+                'name' => 'New Company',
+                'director_full_name' => 'John Doe',
+                'birth_date' => '20/04/1981',
+                'country_id' => '2',
+                'address' => 'John Street 3',
+            ]
+        );
 
-    //     $newCredentials = [
-    //         'name' => 'New Company and Co',
-    //         'director_full_name' => 'Johny Doe',
-    //         'birth_date' => '21/04/1981',
-    //         'country_id' => '3',
-    //         'address' => 'John Street 4',
-    //     ];
+        $newCredentials = [
+            'name' => 'New Company and Co',
+            'director_full_name' => 'Johny Doe',
+            'birth_date' => '21/04/1981',
+            'country_id' => '3',
+            'address' => 'John Street 4',
+        ];
         
-    //     $response = $this->put('api/legal-entities/'.$legalEntity->id, $newCredentials);
+        $response = $this->put('api/legal-entities/'.$legalEntity->id, $newCredentials);
 
-    //     $response->assertStatus(200);
+        $response->assertStatus(200);
 
-    //     $this->assertDatabaseHas('legal_entities', [
-    //         'id' => $legalEntity->id,
-    //         'name' => 'New Company and Co',
-    //         'director_full_name' => 'Johny Doe',
-    //         'birth_date' => '1981-04-21 00:00:00',
-    //         'country_id' => '3',
-    //         'address' => 'John Street 4',
-    //     ]); // Assert that the data is stored in the database
+        $this->assertDatabaseHas('legal_entities', [
+            'id' => $legalEntity->id,
+            'name' => 'New Company and Co',
+            'director_full_name' => 'Johny Doe',
+            'birth_date' => '1981-04-21 00:00:00',
+            'country_id' => '3',
+            'address' => 'John Street 4',
+        ]); // Assert that the data is stored in the database
     
-    // }
+    }
+
+    public function testStockUpdate()
+    {  
+        $legalEntity = LegalEntity::factory()->create(
+            [
+                'name' => 'New Company',
+                'address' => 'John Street 3',
+                'stock' => 1,
+                'iban' => 'LI61 0881 1010 2720 K000 U',
+                'bank_account' => '0102720001000840',
+                'bank_name' => 'Good Bank',
+                'swift' => 'BFRILI22XXX',
+                'account_code' => '001',
+            ]
+        );
+
+        $newCredentials = [
+            'name' => 'New Company Upd',
+            'address' => 'John Street 3 Upd',
+            'stock' => 1,
+            'iban' => 'LI61 0881 1010 2720 W000 U',
+            'bank_account' => '1102720001000840',
+            'bank_name' => 'Good Bank Upd',
+            'swift' => 'BFRILI22YYY',
+            'account_code' => '002',
+        ];
+        
+        $response = $this->put('api/legal-entities/'.$legalEntity->id, $newCredentials);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('legal_entities', [
+            'id' => $legalEntity->id,
+            'name' => 'New Company Upd',
+            'address' => 'John Street 3 Upd',
+            'stock' => 1,
+            'iban' => 'LI61 0881 1010 2720 W000 U',
+            'bank_account' => '1102720001000840',
+            'bank_name' => 'Good Bank Upd',
+            'swift' => 'BFRILI22YYY',
+            'account_code' => '002',
+        ]); // Assert that the data is stored in the database
+    
+    }
 
     // // testing destroy method
     // public function testAddedUserDestroy()
