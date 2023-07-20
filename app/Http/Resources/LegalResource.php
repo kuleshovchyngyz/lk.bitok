28b18035cc8536b76b5e4ddc169dae8962976bf3
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-07-20 12:25:00 +0600

    stocks added to legal_entities (#93)
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- app/Http/Resources/LegalResource.php
+++ app/Http/Resources/LegalResource.php
@@ -15,30 +15,62 @@ class LegalResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id' => class_basename($this->resource) == 'BlackListsLegalEntity' ? '9999999' . $this->id : $this->id,
-            'black_list' => class_basename($this->resource) == 'BlackListsLegalEntity' || BlackList::where('hash', $this->hash)->count() > 0,
-            'sanction' => $this->sanction,
-            'name' => $this->name,
-            'hash' => $this->hash,
-            'director_full_name' => $this->director_full_name,
-            'address' => $this->address,
-            'birth_date' => $this->birth_date?->format('d/m/Y'),
-            'verification' => $this->verification,
-            'registration_date' => $this->created_at->format('d/m/Y H:i'),
-            'verification_date' => $this->when(isset($this->verification_date), function () {
-                return $this->verification_date->format('d/m/Y');
-            }),
-            'country_id' => $this->country_id,
-            'country' => $this->when(isset($this->country), function () {
-                return $this->country->name;
-            }),
-
-            'user_operations' => UserOperationWalletResource::collection($this->whenLoaded('userOperations', function () {
-                return $this->userOperations->unique('wallet_id');
-            })),
-
-            'attachments' => $this->attachments,
-        ];
+        if (!$this->stock) {
+            return [
+                'id' => class_basename($this->resource) == 'BlackListsLegalEntity' ? '9999999' . $this->id : $this->id,
+                'black_list' => class_basename($this->resource) == 'BlackListsLegalEntity' || BlackList::where('hash', $this->hash)->count() > 0,
+                'sanction' => $this->sanction,
+                'name' => $this->name,
+                'hash' => $this->hash,
+                'director_full_name' => $this->director_full_name,
+                'address' => $this->address,
+                'birth_date' => $this->birth_date?->format('d/m/Y'),
+                'verification' => $this->verification,
+                'registration_date' => $this->created_at->format('d/m/Y H:i'),
+                'verification_date' => $this->when(isset($this->verification_date), function () {
+                    return $this->verification_date->format('d/m/Y');
+                }),
+                'country_id' => $this->country_id,
+                'country' => $this->when(isset($this->country), function () {
+                    return $this->country->name;
+                }),
+    
+                'user_operations' => UserOperationWalletResource::collection($this->whenLoaded('userOperations', function () {
+                    return $this->userOperations->unique('wallet_id');
+                })),
+    
+                'attachments' => $this->attachments,
+            ];
+        } else {
+            return [
+                'id' => class_basename($this->resource) == 'BlackListsLegalEntity' ? '9999999' . $this->id : $this->id,
+                'black_list' => class_basename($this->resource) == 'BlackListsLegalEntity' || BlackList::where('hash', $this->hash)->count() > 0,
+                'sanction' => $this->sanction,
+                'name' => $this->name,
+                'hash' => $this->hash,
+                'address' => $this->address,
+                'stock' => $this->stock,
+                'iban' => $this->iban,
+                'bank_account' => $this->bank_account,
+                'bank_name' => $this->bank_name,
+                'swift' => $this->swift,
+                'account_code' => $this->account_code,
+                'verification' => $this->verification,
+                'registration_date' => $this->created_at->format('d/m/Y H:i'),
+                'verification_date' => $this->when(isset($this->verification_date), function () {
+                    return $this->verification_date->format('d/m/Y');
+                }),
+                'country_id' => $this->country_id,
+                'country' => $this->when(isset($this->country), function () {
+                    return $this->country->name;
+                }),
+    
+                'user_operations' => UserOperationWalletResource::collection($this->whenLoaded('userOperations', function () {
+                    return $this->userOperations->unique('wallet_id');
+                })),
+    
+                'attachments' => $this->attachments,
+            ];
+        }
     }
 }
