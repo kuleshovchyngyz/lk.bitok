9a4a76e613efac52a1d0c90f715a5e72ab5777e8
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-07-21 17:18:27 +0600

    Ciwork (#102)
    * test again
    
    * changed workflow from roles to ci
    
    * again
    
    * test
    
    * test again
    
    * changed date
    
    * date format problems
    
    * dates
    
    * dates
    
    * dates again
    
    * hash faker
    
    * useroper date fixed
    
    * migrate spatie
    
    * composer update to run spatie migrations
    
    * spatie permissions with vendor publish
    
    * modified Role and permission seeder
    
    * blabla
    
    * haha
    
    * changed user in seeder for authorization
    
    * some fix
    
    * modified user seeder
    
    * fixing
    
    * some modification
    
    * checking for data
    
    * check for roles
    
    * enable role assigning
    
    * test again
    
    * check the tables in DB
    
    * hello
    
    * again
    
    * lets try again
    
    * set up the DB for local testing
    
    * sth sth
    
    * blabla
    
    * changed credentials
    
    * .env.example local to testing
    
    * added countrySeeder
    
    * countrySeeder on
    
    * modified addedUsersTest and added LegalEntityTest
    
    * sth
    
    * improving
    
    * legalEntityTest finshed, userTest added
    
    * added userOperationTest
    
    * userOperationTest modified
    
    * CountryTest added
    
    * SettingsTest added
    
    ---------
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

---
+++ tests/Feature/SettingsTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Setting;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SettingsTest extends TestCase
+{
+    use DatabaseTransactions;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Authenticate a user
+        $user = User::where('name','AdminTester')->first();
+        $this->actingAs($user);
+    }
+
+    // test index method
+    public function testSettingsIndex()
+    {
+        $response = $this->get('api/settings');
+
+        $response->assertStatus(200);
+    }
+
+    // testing store method
+    public function testSettingsStore()
+    {  
+        $credentials = [
+            'limit' => 1000,
+            'usd_to_som' => 88,
+            'usdt_to_som' => 88,
+            'rub_to_som' => 1.05,
+            'high_risk' => 365,
+            'risk' => 1000,
+        ];
+        
+        $response = $this->postJson('api/settings', $credentials);
+        
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('settings', [
+            'limit' => 1000,
+            'usd_to_som' => 88,
+            'usdt_to_som' => 88,
+            'rub_to_som' => 1.05,
+            'high_risk' => 365,
+            'risk' => 1000,
+        ]); // Assert that the data is stored in the database
+    }
+
+    // testing store method
+    public function testIfSettingsStoreNotAddingButUpdating()
+    {  
+        $credentials = [
+            'limit' => 1000,
+            'usd_to_som' => 88,
+            'usdt_to_som' => 88,
+            'rub_to_som' => 1.05,
+            'high_risk' => 365,
+            'risk' => 1000,
+        ];
+        
+        $response = $this->postJson('api/settings', $credentials);
+       
+        $this->assertDatabaseHas('settings', [
+            'limit' => 1000,
+            'usd_to_som' => 88,
+            'usdt_to_som' => 88,
+            'rub_to_som' => 1.05,
+            'high_risk' => 365,
+            'risk' => 1000,
+        ]);
+
+        $rowCount = Setting::count();
+
+        $this->assertEquals(1, $rowCount);
+    }
+}
