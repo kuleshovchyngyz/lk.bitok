4d34cc08652a6567e14251a8852e1c6e556bd415
Azamat <azamailnet@mail.ru>
2024-03-20 15:37:38 +0600

    swagger protected by basic auth

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -61,7 +61,7 @@ return [
              * Middleware allows to prevent unexpected access to API documentation
             */
             'middleware' => [
-                'api' => [],
+                'api' => ['auth.basic'],
                 'asset' => [],
                 'docs' => [],
                 'oauth2_callback' => [],
